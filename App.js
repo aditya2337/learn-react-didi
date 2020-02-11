21c427b620f71ef33dd8c5a6bd21736f0e1518ad
Aditya <androiditya@gmail.com>
2020-02-11 21:07:49 +0530

    adding react using parcel

--- App.js
+++ App.js
@@ -1,28 +1,57 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 const rootDiv = document.getElementById("root");
 
-// const App = React.createElement("div", {
-//   children: React.createElement("input", {
-//     disabled: false,
-//   }),
+// // const App = React.createElement("div", {
+// //   children: React.createElement("input", {
+// //     disabled: false,
+// //   }),
+// // });
+
+// // const App = React.createElement("ul", {
+// //   children: Array.from({ length: 6 }).map((val, idx) =>
+// //     React.createElement("li", { children: `Hello ${idx}`, key: idx }),
+// //   ),
+// // });
+
+// let value = "";
+// const Input = React.createElement("input", {
+//   onChange: onChangeHandler,
+// });
+// const Button = React.createElement("button", {
+//   type: "submit",
+//   children: "save"
+// });
+// const ButtonContainer = React.createElement("div", {
+//   children: Button,
+// });
+// const Form = React.createElement("form", {
+//   onSubmit: submitHandler,
+//   children: [Input,  ButtonContainer]
 // });
 
-const App = React.createElement("ul", {
-  children: Array.from({ length: 6 }).map((val, idx) =>
-    React.createElement("li", { children: `Hello ${idx}`, key: idx }),
-  ),
-});
+// ReactDOM.render(Form, rootDiv);
 
-ReactDOM.render(App, rootDiv);
 
-// const input = document.createElement("input");
+// function submitHandler(e) {
+//   e.preventDefault();
+//   console.log(value);
+// }
 
-// input.disabled = true;
+// function onChangeHandler(e) {
+//   value = e.target.value;
+// }
 
-// rootDiv.appendChild(input);
+// // const input = document.createElement("input");
 
-//
+// // input.disabled = true;
 
-"hello"
+// // rootDiv.appendChild(input);
+
+// //
+
+// // "hello"
 
 `
   // Print the value in console on submit
@@ -33,3 +62,15 @@ ReactDOM.render(App, rootDiv);
     </div>
   </form>
 `;
+
+function App() {
+  return (
+    <h1>
+      Rendered
+      <input type="text" value="adi" onChange={() => ""} />
+    </h1>
+  )
+}
+
+
+ReactDOM.render(<App />, rootDiv);
