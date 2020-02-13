3762ad51a585e4de77f0bebc58deeb996107a9c7
Aditya <androiditya@gmail.com>
2020-02-13 21:05:52 +0530

    react states

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 
 const rootDiv = document.getElementById("root");
 
+
 // // const App = React.createElement("div", {
 // //   children: React.createElement("input", {
 // //     disabled: false,
@@ -63,13 +64,35 @@ const rootDiv = document.getElementById("root");
   </form>
 `;
 
-function App() {
-  return (
-    <h1>
-      Rendered
-      <input type="text" value="adi" onChange={() => ""} />
-    </h1>
-  )
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      value: "as",
+    }
+
+    this.onChangeHandler = this.onChangeHandler.bind(this);
+  }
+
+  onChangeHandler(e) {
+    const { value } = e.target;
+
+    this.setState({
+      value
+    });
+  }
+
+  render() {
+    const { value } = this.state;
+
+    return (
+      <form>
+        <input type="text" value={value} onChange={this.onChangeHandler} />
+        <button type="submit">Save</button>
+      </form>
+    )
+  }
 }
 
 
